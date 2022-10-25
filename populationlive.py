582e62b5e80546dfc33883c20686376e115dc651
Jek20 <92286337+Jek20@users.noreply.github.com>
2022-10-25 11:40:48 +0700

    Update populationlive.py

--- populationlive.py
+++ populationlive.py
@@ -40,7 +40,7 @@ def main():
     if st.button('Population Test Result'):
         Live = population_prediction(Area, Density, GrowthRate, Rank, Population)
    
-    if st.success(Live)
+    st.success()
     
 if __name__ == '__main__':
     main()
